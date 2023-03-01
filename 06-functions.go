b7ccdf2ff4707b7ca1b8f69bdb5dacc332b80a43
prasanth-ntu <prasanththegalaxian@gmail.com>
2023-03-01 10:20:32 +0800

    Add 06-functions.go file

---
+++ 06-functions.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func countDown(num int) {
+	for i := num; i > 0; i-- {
+		fmt.Println(i)
+	}
+}
+
+func addNumber(num1 int, num2 int) int {
+	// return num1 + num2
+	sum := num1 + num2
+	return sum
+}
+
+func main() {
+	fmt.Println("Intro to functions")
+
+	countDown(10)
+	countDown(5)
+
+	theSum := addNumber(5, 10)
+	fmt.Println("Sum of two numbers:", theSum)
+}
