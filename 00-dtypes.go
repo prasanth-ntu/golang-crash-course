68baa732209f4e301b717889d323cd9edd7be7a1
prasanth-ntu <prasanththegalaxian@gmail.com>
2023-03-02 09:10:25 +0800

    Add 00-dtypes.go file

---
+++ 00-dtypes.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// Data types
+	// string
+	// bool
+	// int		int8 	int16 	int32 	int64
+	// uint	uint8	uint16 	uint32 	uint64 	uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32	flot64
+	// complex64 complex128
+
+}
