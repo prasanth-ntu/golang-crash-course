daf2d1bca4424b228506bb88f6389a167e5ca07d
prasanth-ntu <prasanththegalaxian@gmail.com>
2023-03-04 12:44:58 +0800

    Add additional example for pointers

---
+++ 11b-pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := &a // Assigning b to pointer of a (memory address of where the value is stored)
+	fmt.Printf("a=%T, b=%T\n", a, b)
+	fmt.Println(a, b)
+
+	// Read the value from memory address using *
+	fmt.Println(a, *b)
+	fmt.Println(a, &a, *&a)
+
+	// Change value with pointer
+	*b = 5
+	fmt.Println(a, b)
+
+	a = 15 // b still points to the same address
+	fmt.Println(a, b)
+}
