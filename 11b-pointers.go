5a37535981a2e261003e7aaf6a2f52db8c94cd48
prasanth.thangavel <prasanth.thangavel@grabtaxi.com>
2023-09-01 13:59:53 +0800

    Minor updates to pointers

--- 11b-pointers.go
+++ 11b-pointers.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
 	a := 10
 	b := &a // Assigning b to pointer of a (memory address of where the value is stored)
-	fmt.Printf("a=%T, b=%T\n", a, b)
+
+	//fmt.Printf("a=%T, b=%T\n", a, b)
+	fmt.Println(reflect.TypeOf(a), reflect.TypeOf(b))
+
 	fmt.Println(a, b)
 
 	// Read the value from memory address using *
@@ -19,4 +25,5 @@ func main() {
 
 	a = 15 // b still points to the same address
 	fmt.Println(a, b)
+
 }
