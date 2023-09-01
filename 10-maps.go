598885dd13e2e44c9bebcc41697a2f146b6a0546
prasanth.thangavel <prasanth.thangavel@grabtaxi.com>
2023-09-01 13:49:03 +0800

    Minor updates to maps

--- 10-maps.go
+++ 10-maps.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	/*
+		Array vs. Slice vs. Map
+		- Array: Fixed size, Ordered, Accessible by index
+			e.g., var a [5]int
+		- Slice: Dynamic size, Ordered, Accessible by index
+			e.g., var s []int
+		- Map: Dynamic size, Unordered collection, Accessible by key as they are key-value pairs. Maps are the equivalent of Python's dictionary.
+			e.g., m = make(map[string]int)
+	*/
 	cart := make(map[string]int)
 
-	// Wwite //
+	// Write //
 	cart["lamp"] = 3
 	cart["bowl"] = 1
 	cart["laptop"] += 1 // Since we haven't created laptop key before, it will assign 0 as a default value and perform addition
@@ -54,5 +66,12 @@ func main() {
 	for key, value := range emails2 {
 		fmt.Printf("Key: %s => Value: %s\n", key, value)
 	}
+
+	// Check if a key exists in the map //
+	_, ok := emails2["Santh"]
+	fmt.Println("Is 'Santh' in the map?", ok)
+	// If the value is not present, it will return the default value of the data type //
+	value, ok := emails2["Santh2"]
+	fmt.Println("Is 'Santh2' in the map?", ok, ". The corresponding value is", value, "and the type of 'value' is", reflect.TypeOf(value))
 
 }
