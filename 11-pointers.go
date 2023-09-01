9c70a8209935443ae24aef712056ab3c5b3770c9
prasanth.thangavel <prasanth.thangavel@grabtaxi.com>
2023-09-01 13:57:30 +0800

    Minor updates to pointers

--- 11-pointers.go
+++ 11-pointers.go
@@ -14,9 +14,17 @@ func main() {
 	// With pointers //
 	var c = 1
 	var d *int // d is a pointer
-	d = &c     // d is pointing to c
-	fmt.Println(c, d)
+	d = &c     // d is pointing to c (i.e., memory address of c
+	/*
+		Explanation:
+			- c: the value of c,
+			- *d: value at the memory address that d points to,
+			- d: memory address stored in d,
+			- &c: memory address of c,
+			- &d: memory address of d.
+	*/
+	fmt.Println(c, *d, d, &c, &d)
 
 	*d += 1 // Since d is pointing to c, original variable stored in memory
-	fmt.Println(c, d)
+	fmt.Println(c, *d, d, &c, &d)
 }
