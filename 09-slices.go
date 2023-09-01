60ede722844f7b167dac17fb1278d5a1665d22f3
prasanth.thangavel <prasanth.thangavel@grabtaxi.com>
2023-09-01 13:26:41 +0800

    Minor updates to slices

--- 09-slices.go
+++ 09-slices.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
+	/*
+		Arrays vs Slices
+		- Arrays are fixed in size, whereas slices are dynamic (so, more flexible)
+		- Arrays to Slices and vice versa
+			- To convert an array to slice, we can use [<start>:<end>] operator. Both <start> and <end> are optional
+			- To convert a slice to array, we can use a for loop or "copy" function
+		- append() function is used to add elements to the end of slice
+	*/
+
 	// Array approach limitation: The size of the array is fixed //
 	purchases := [5]float32{20.1, 21.1, 22.2, 21.3, 22.4}
 	fmt.Println("purchases:", purchases)
@@ -30,4 +39,18 @@ func main() {
 
 	combine := append(mySlice, myOtherSlice...)
 	fmt.Println(combine)
+
+	var myarray, myarray2 [5]float32
+
+	// Convert slice to array: Approach 1
+	for i := 0; i < len(purchases2); i++ {
+		myarray[i] = purchases2[i]
+	}
+	fmt.Println("\nmyarray:", myarray)
+	fmt.Println("TypeOf(myarray):", reflect.TypeOf(myarray))
+
+	// Convert slice to array: Approach 1
+	copy(myarray2[:], purchases2)
+	fmt.Println("\nmyarray2:", myarray2)
+	fmt.Println("TypeOf(myarray2):", reflect.TypeOf(myarray2))
 }
